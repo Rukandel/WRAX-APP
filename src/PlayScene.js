b98327d7d466805632197f82510b5c31b7c3aa66
Nikita Usachev <guyfoks737@gmail.com>
2024-05-08 18:34:42 +0300

    second commit

---
+++ src/PlayScene.js
@@ -0,0 +1,153 @@
+import Phaser from "phaser";
+
+class PlayScene extends Phaser.Scene {
+  constructor() {
+    super("PlayScene");
+  }
+
+  create() {
+    this.isGameRunning = false;
+    this.gameSpeed = 10;
+    this.respawnTime = 0;
+
+    const { height, width } = this.game.config;
+
+    this.startTrigger = this.physics.add
+      .sprite(0, 10)
+      .setOrigin(0, 1)
+      .setImmovable();
+    this.ground = this.add
+      .tileSprite(0, height, 88, 26, "ground")
+      .setOrigin(0, 1);
+    this.dino = this.physics.add
+      .sprite(0, height, "dino-idle")
+      .setOrigin(0, 1)
+      .setOrigin(0, 1)
+      .setCollideWorldBounds(true)
+      .setGravityY(5000);
+
+    this.obsticles = this.physics.add.group();
+
+    this.initAnim();
+    this.handleInputs();
+    this.initStartTrigger();
+  }
+
+  initStartTrigger() {
+    const { width, height } = this.game.config;
+    this.physics.add.overlap(
+      this.startTrigger,
+      this.dino,
+      () => {
+        if (this.startTrigger.y === 10) {
+          this.startTrigger.body.reset(0, height);
+          return;
+        }
+
+        this.startTrigger.disableBody(true, true);
+
+        const startEvent = this.time.addEvent({
+          delay: 1000 / 60,
+          loop: true,
+          callbackScope: this,
+          callback: () => {
+            this.dino.setVelocityX(80);
+            this.dino.anims.play("dino-run", 1);
+
+            if (this.ground.width < width) {
+              this.ground.width += 17 * 2;
+            }
+
+            if (this.ground.width >= width) {
+              this.ground.width = width;
+              this.isGameRunning = true;
+              this.dino.setVelocity(0);
+              startEvent.remove();
+            }
+          },
+        });
+      },
+      null,
+      this
+    );
+  }
+
+  initAnim() {
+    this.anims.create({
+      key: "dino-run",
+      frames: this.anims.generateFrameNumbers("dino", {
+        start: 2,
+        end: 3,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "dino-down-anim",
+      frames: this.anims.generateFrameNumbers("dino-down", {
+        start: 0,
+        end: 1,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "enemy-dino-fly",
+      frames: this.anims.generateFrameNumbers("enemy-bird", {
+        start: 0,
+        end: 1,
+      }),
+      frameRate: 6,
+      repeat: -1,
+    });
+  }
+
+  handleInputs() {
+    this.input.keyboard.on("keydown-SPACE", () => {
+      if (!this.dino.body.onFloor()) {
+        return;
+      }
+
+      this.dino.body.height = 92;
+      this.dino.body.offset.y = 0;
+
+      this.dino.setVelocityY(-1600);
+    });
+
+    this.input.keyboard.on("keydown-DOWN", () => {
+      if (!this.dino.body.onFloor()) {
+        return;
+      }
+      this.dino.body.height = 58;
+      this.dino.body.offset.y = 34;
+    });
+
+    this.input.keyboard.on("keyup-DOWN", () => {
+      if (!this.dino.body.onFloor()) {
+        return;
+      }
+      this.dino.body.height = 92;
+      this.dino.body.offset.y = 0;
+    });
+  }
+
+  update() {
+    if (!this.isGameRunning) return;
+    this.ground.tilePositionX += this.gameSpeed;
+
+    if (this.dino.body.deltaAbsY() > 0) {
+      this.dino.anims.stop();
+      this.dino.setTexture("dino");
+    } else {
+      this.dino.anims.play("dino-run", true);
+
+      this.dino.body.height <= 58
+        ? this.dino.anims.play("dino-down-anim", true)
+        : this.dino.anims.play("dino-run", true);
+    }
+  }
+}
+
+export default PlayScene;
--- src/main.js
+++ src/main.js
@@ -1,28 +1,21 @@
-import { Boot } from './scenes/Boot';
-import { Game } from './scenes/Game';
-import { GameOver } from './scenes/GameOver';
-import { MainMenu } from './scenes/MainMenu';
-import { Preloader } from './scenes/Preloader';
+import Phaser from "phaser";
 
-//  Find out more information about the Game Config at:
-//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+import PlayScene from "./PlayScene";
+import PreloadScene from "./PreloadScene";
+
 const config = {
-    type: Phaser.AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
+  type: Phaser.AUTO,
+  width: 1000,
+  height: 340,
+  pixelArt: true,
+  transparent: true,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
     },
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        Game,
-        GameOver
-    ]
+  },
+  scene: [PreloadScene, PlayScene],
 };
 
-export default new Phaser.Game(config);
+new Phaser.Game(config);
